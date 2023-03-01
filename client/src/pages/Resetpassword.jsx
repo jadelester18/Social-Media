8388d9ef0a13b23576aeaf6092dfd10965f3e351
DaisyS22 <rogelioumbay.ru@gmail.com>
2023-03-01 10:32:09 +0800

    reset password / verify email

--- client/src/pages/Resetpassword.jsx
+++ client/src/pages/Resetpassword.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import {
+  Button,
+  Card,
+  CardMedia,
+  Container,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { Stack } from '@mui/system';
+import React, { useState } from 'react';
 
-const Resetpassword = () => {
-  return <div>Resetpassword</div>;
-};
+export default function Resetpassword() {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
-export default Resetpassword;
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Card
+        sx={{
+          margin: '150px auto',
+          width: '450px',
+          padding: '30px',
+          boxShadow: '5',
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="auto"
+          image="https://img.freepik.com/premium-vector/landing-page-illustration-design-people-forgot-her-password_108061-334.jpg?w=1380"
+          title="OTP image"
+          sx={{ marginBottom: '30px' }}
+        />
+        <Stack
+          spacing={3}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h6">Enter your new password</Typography>
+          <form>
+            <TextField
+              fullWidth
+              name="password"
+              label="Password"
+              type={showPassword ? 'text' : 'password'}
+              id="standard-adornment-password"
+              autoComplete="new-password"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              // onChange={(e) => setpassword(e.target.value)}
+            />
+            <Button
+              sx={{ marginTop: '20px' }}
+              variant="contained"
+              fullWidth
+              // onClick={handleClick}
+            >
+              Set Password
+            </Button>
+          </form>
+        </Stack>
+      </Card>
+    </Container>
+  );
+}
--- client/src/pages/Verifyemail.jsx
+++ client/src/pages/Verifyemail.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import {
+  Button,
+  Card,
+  CardMedia,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Verifyemail = () => {
-  return <div>Verifyemail</div>;
-};
-
-export default Verifyemail;
+export default function Verifyemail() {
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Card
+        sx={{
+          margin: '150px auto',
+          width: '450px',
+          padding: '30px',
+          boxShadow: '5',
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="auto"
+          image="https://img.freepik.com/free-vector/enter-otp-concept-illustration_114360-7867.jpg?w=1380&t=st=1677556310~exp=1677556910~hmac=7317addb689575d129fd0b45d705983a69b94ae8b01363eb03ea7568ccf7e17a"
+          title="OTP image"
+          sx={{ marginBottom: '30px' }}
+        />
+        <Stack
+          spacing={2}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h6">
+            The OTP has been sent to your email!
+          </Typography>
+          <form>
+            <TextField
+              type="number"
+              placeholder="Enter Your OTP"
+              size="small"
+              fullWidth
+              sx={{ paddingTop: '20px' }}
+              // onChange={(e) => setOTP(e.target.value)}
+            />
+            <Button
+              sx={{ margin: '20px auto' }}
+              variant="contained"
+              fullWidth
+              // onClick={handleOTP}
+            >
+              Confirm OTP
+            </Button>
+            <Link
+              to={'/register'}
+              style={{ textDecoration: 'none', color: 'black' }}
+            >
+              <Typography>Check your email for the OTP</Typography>
+            </Link>
+          </form>
+        </Stack>
+      </Card>
+    </Container>
+  );
+}
