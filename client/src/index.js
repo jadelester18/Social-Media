9d7a01f64ff95fbdb80925598fc2359bed890777
Rowell Esguerra <wel.esguerra@gmail.com>
2023-03-08 10:54:06 +0800

    try

--- client/src/index.js
+++ client/src/index.js
@@ -22,3 +22,26 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+const express = require("express")
+const app = express();
+const mongo = require("mongoose")
+const dotenv = require("dotenv")
+const cors = require("cors");
+const userRouter = require("./router/user");
+const postRouter = require("./router/post");
+dotenv.config();
+
+mongo
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Mongodb connection successful");
+  })
+  .catch(() => {
+    console.log("Something went wrong.");
+  });
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
+
+app.listen(5000, () => console.log("Server running"))
